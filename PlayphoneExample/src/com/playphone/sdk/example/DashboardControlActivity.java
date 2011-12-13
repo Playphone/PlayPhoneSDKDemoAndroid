34256749ae3db42334eac99e5b9ad361c53a2a49
Pavel Lyashko <pavell@playphone.com>
2011-12-13 16:06:43 +0200

    was improved dashboard control

--- PlayphoneExample/src/com/playphone/sdk/example/DashboardControlActivity.java
+++ PlayphoneExample/src/com/playphone/sdk/example/DashboardControlActivity.java
@@ -4,72 +4,105 @@ import com.playphone.multinet.MNDirect;
 import com.playphone.multinet.MNDirectUIHelper;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.AdapterView.OnItemClickListener;
 
-public class DashboardControlActivity extends CustomTitleActivity {
+public class DashboardControlActivity extends CustomTitleListActivity {
+
+	private static class DashboardCallItem {
+
+		public DashboardCallItem(String name, String command) {
+			this.name = name;
+			this.command = command;
+			this.param = null;
+		}
+
+		public DashboardCallItem(String name, String command, String param) {
+			this.name = name;
+			this.command = command;
+			this.param = param;
+		}
+
+		public String name;
+		public String command;
+		public String param;
+	}
+
+	DashboardCallItem[] itemList = {
+			new DashboardCallItem("Leaderboards", "jumpToLeaderboard"),
+			new DashboardCallItem("Friend list", "jumpToBuddyList"),
+			new DashboardCallItem("User Profile", "jumpToUserProfile"),
+			new DashboardCallItem("User Home", "jumpToUserHome"),
+			new DashboardCallItem("Achievements", "jumpToAchievements"),
+			new DashboardCallItem("Game Info", "jumpToGameInfo"),
+			new DashboardCallItem("Add Friends", "jumpToAddFriends"),
+			new DashboardCallItem("PlayCredits Shop", "jumpToGameShop", "_credits"),
+			new DashboardCallItem("Redeem", "jumpToGameShop", "_redeem"),
+			new DashboardCallItem("Shop catalog", "jumpToGameShop") };
+
+	ArrayAdapter<DashboardCallItem> aa = null;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dashboard_control);
-	
+
 		// set the breadcrumbs text
 		TextView txtBreadCrumbs = (TextView) findViewById(R.id.txtBreadCrumbs);
 		txtBreadCrumbs.setText("Home > Dashboard Control");
-		
-		Button btnLoginScreen = (Button) findViewById(R.id.btnLoginScreen);
-		btnLoginScreen.setOnClickListener(new OnClickListener() {
+
+		aa = new ArrayAdapter<DashboardCallItem>(DashboardControlActivity.this,
+				R.layout.dashboard_control_item, itemList) {
+
 			@Override
-			public void onClick(View v) {
-				MNDirect.execAppCommand("jumpToUserLogin",null);
+			public View getView(int position, View convertView, ViewGroup parent) {
+				View row = convertView;
+
+				if (row == null) {
+					LayoutInflater li = getLayoutInflater();
+					row = li.inflate(R.layout.dashboard_control_item, null);
+				}
+				DashboardCallItem i = itemList[position];
+				((TextView) row.findViewById(R.id.name)).setText(i.name);
+				StringBuilder info = new StringBuilder(i.command);
+				if (i.param != null) {
+					info.append(" : ").append(i.param);
+				}
+				((TextView) row.findViewById(R.id.command)).setText(info
+						.toString());
+				return row;
+			}
+		};
+
+		setListAdapter(aa);
+
+		getListView().setOnItemClickListener(new OnItemClickListener() {
+			@Override
+			public void onItemClick(AdapterView<?> paramAdapterView,
+					View paramView, int position, long paramLong) {
+
+				DashboardCallItem i = itemList[position];
+
+				MNDirect.execAppCommand(i.command, i.param);
 				MNDirectUIHelper.showDashboard();
 			}
 		});
-		
-		Button btnLeaderboards = (Button) findViewById(R.id.btnLeaderboards);
-		btnLeaderboards.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				MNDirect.execAppCommand("jumpToLeaderboard",null);
-				MNDirectUIHelper.showDashboard();
-			}
-		});
-
-		
-		Button btnAchievements = (Button) findViewById(R.id.btnAchievements);
-		btnAchievements.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				MNDirect.execAppCommand("jumpToAchievements",null);
-				MNDirectUIHelper.showDashboard();
-			}
-		});
-
-
-		/*
-		Button btnStore = (Button) findViewById(R.id.btnStore);
-		btnStore.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				MNDirect.execAppCommand("jumpToStore",null);
-				MNDirectUIHelper.showDashboard();
-			}
-		});
-		*/
-
-		Button btnHome = (Button) findViewById(R.id.btnHome);
-		btnHome.setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				MNDirect.execAppCommand("jumpToUserHome",null);
-				MNDirectUIHelper.showDashboard();
-			}
-		});
-
-	
 	}
-		
 
+	@Override
+	protected void onResume() {
+		super.onResume();
+		MNDirectUIHelper.setHostActivity(this);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		MNDirectUIHelper.setHostActivity(null);
+	}
 }
